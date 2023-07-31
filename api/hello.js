22108fed954b3f8dd80baf4142353f5b3cca29b2
Harsha Goel <harshagoyal868@gmail.com>
2023-07-31 13:55:33 +0530

    four commit

--- api/hello.js
+++
@@ -1,9 +0,0 @@
-// hello.js
-
-module.exports = (req, res) => {
-    // Perform any backend logic here (if needed)
-  
-    // Respond with a JSON object
-    res.status(200).json({ message: "Hello from the serverless function!" });
-  };
-  
